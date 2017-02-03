21b841904c21af895d048a625dea56d8d2fde836
Miguel Ángel <maletils@gmail.com>
2017-02-04 00:10:09 +0100

    Prueba de diseño para página About

---
+++ about.php
@@ -0,0 +1,31 @@
+<!-- Handwritten by Maletil. 2015-17 -->
+<?php
+/**
+ * Created with PhpStorm.
+ * User: USUARIO
+ * Date: 28/01/2017
+ * Time: 20:02
+ */
+?>
+<head>
+    <link rel="stylesheet" style="text/css" href="css/about.css"/>
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0">
+    <meta charset="UTF-8"/>
+    <link rel="icon" href="img/favicon.png"/>
+    <title>Maletil - About</title>
+</head>
+<body>
+<header>
+    <ul>
+        <li>Inicio</li>
+        <li>About</li>
+        <li>Contacto</li>
+    </ul>
+</header>
+<div class="header">
+    <h1>Maletil</h1>
+</div>
+    <div class="separator">
+
+    </div>
+</body>
